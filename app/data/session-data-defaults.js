57c4736bcc06cc69cc0be54641df6271b033da2e
garethdavidthomas <52539673+garethdavidthomas@users.noreply.github.com>
2023-09-14 09:02:48 +0100

    FTF Agreement coded prototypes

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -2,4 +2,39 @@ module.exports = {
 
   // Insert values here
 
+  // RPS account data
+  'applicant-name': 'John Smith',
+  'business-name': 'Imaginary Farms Ltd',
+  'business-sbi-number': '357951123',
+
+
+  // FTF application data
+
+  'ftf-grant-name': 'Water Management Grant - Round 3 ',
+  'ftf-project-name': 'Browns Hill Farm reservoir',
+  'ftf-project-reference': 'WM-VMW-1234',
+
+
+  '01-01': 'ready',
+  '01-02': 'ready',
+  '01-03': 'ready',
+
+  '02-01': 'ready',
+  '02-02': 'ready',
+  '02-03': 'ready',
+  '02-04': 'ready',
+  
+  '03-01': 'ready',
+  '03-02': 'ready',
+  '03-03': 'ready',
+
+  '04-01': 'ready',
+  '04-02': 'ready',
+  '04-03': 'ready',
+  '04-04': 'ready'
+
+  // FETF application data
+
+  // Activity List statuses
+
 }
