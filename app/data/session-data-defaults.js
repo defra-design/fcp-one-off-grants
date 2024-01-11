3ca5d6b384d83915c7ffa253b267c7211f748f8a
garethdavidthomas <52539673+garethdavidthomas@users.noreply.github.com>
2024-01-11 11:46:16 +0000

    FETF Activity List first draft

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -18,13 +18,19 @@ module.exports = {
   'fetf-grant-name': 'Farming Equipment and Technology Fund 2023',
   'fetf-project-reference': 'FETF-VMA-1234',
 
-  // Activity List statuses
+  // FTF Activity List statuses
   'status': '01a',
   'eoi': '01',
   'apply': '01',
   'agree': '01',
   'claim-01': '01',
   'claim-02': '01',
-  'claim-03': '01'
+  'claim-03': '01',
+
+  // FTF Activity List statuses
+  'fetf-status': '01a',
+  'fetf-apply': '01',
+  'fetf-agree': '01',
+  'fetf-claim': '01'
 
 }
