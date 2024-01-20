199e67f95af892c7b8f231dfeb1b8551d1788d37
garethdavidthomas <52539673+garethdavidthomas@users.noreply.github.com>
2024-01-20 08:22:12 +0000

    Activity List - Monitoring designs

---
+++ app/data.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const _ = require('underscore')
+
+const files = fs.readdirSync('./app/data/fetf-items/')
+const data = []
+
+
+// Function below is combining all JSON data files into a function called getData
+
+function getData() {
+    // The loop to merge all JSON files together
+    _.each(files, (el) => {
+    const file = fs.readFileSync('./app/data/fetf-items/' + el).toString()
+    try {
+        const json = JSON.parse(file)
+        json.filename = el
+        data.push(json)
+    } catch(err) {
+        console.log(err)
+    }
+});
+
+    return data;
+}
+
+module.exports = {
+    getData,
+}
--- app/routes.js
+++ app/routes.js
@@ -7,4 +7,3 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
-
