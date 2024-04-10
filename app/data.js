62d49bb8d495340632fafab1190bf596600c0377
Joe Lanman <joelanman@gmail.com>
2024-04-10 15:51:39 +0100

    fix file error

--- app/data.js
+++ app/data.js
@@ -4,21 +4,22 @@ const _ = require('underscore')
 const files = fs.readdirSync('./app/data/equipment/')
 const data = []
 
-
 // Function below is combining all JSON data files into a function called getData
 
 function getData() {
     // The loop to merge all JSON files together
-    _.each(files, (el) => {
-    const file = fs.readFileSync('./app/data/equipment/' + el).toString()
-    try {
-        const json = JSON.parse(file)
-        json.filename = el
-        data.push(json)
-    } catch(err) {
-        console.log(err)
-    }
-});
+    _.each(files, (filename) => {
+            if (filename.endsWith('.json')){
+            const file = fs.readFileSync('./app/data/equipment/' + filename).toString()
+            try {
+                const json = JSON.parse(file)
+                json.filename = filename
+                data.push(json)
+            } catch(err) {
+                console.log('Can\'t read file: ',filename)
+            }
+        }
+    })
 
     return data;
 }
