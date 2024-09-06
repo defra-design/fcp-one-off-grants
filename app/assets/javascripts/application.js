a3af02eb04f80a9e23d7e77e491b0caf4ddfbe17
Sineaddumigan <shdumigan@gmail.com>
2024-09-06 16:14:52 +0100

    Claims journey v1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,14 @@
 
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
+
+
 })
 
 
+
+new MOJFrontend.MultiFileUpload({
+  container: document.querySelector(".moj-multi-file-upload"),
+  uploadUrl: "/ajax-upload",
+  deleteUrl: "/ajax-delete",
+});
